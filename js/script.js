a872e10c71139a151c02a24c69e3e6202bf3467f
GiadaMarzapane <giada.ortesta@gmail.com>
2022-12-19 16:06:50 +0100

    Fix file js

--- js/script.js
+++ js/script.js
@@ -9,17 +9,23 @@ console.log(typeof "L'utente ha: ", myAge);
 let tripPrice = kmChoice * 0.21;
 tripPrice = tripPrice.toFixed(2);
 
+let discountPrice20 = (tripPrice - (tripPrice * 0.20));
+discountPrice20 = discountPrice20.toFixed(2);
+
+let discountPrice40 = (tripPrice - (tripPrice * 0.40));
+discountPrice40 = discountPrice40.toFixed(2);
+
 console.log('Il prezzo del biglietto sarà €', tripPrice);
 console.log(typeof 'Il prezzo base del biglietto sarà €', tripPrice);
 
-if (myAge < '18') {
-    document.getElementById('minorenne').innerHTML= (tripPrice - (tripPrice * 0.20));
-    console.log(tripPrice - (tripPrice * 0.20));
+if (myAge < 18) {
+    document.getElementById('minorenne').innerHTML= discountPrice20;
+    console.log('Prezzo scontato: ', discountPrice20);
     document.getElementById('baseprice').innerHTML = ('al posto di € ' + tripPrice);
 }
 
-if (myAge >= '65') {
-    document.getElementById('over65').innerHTML = (tripPrice - (tripPrice * 0.40));
-    console.log(tripPrice - (tripPrice * 0.40));
+if (myAge >= 65) {
+    document.getElementById('over65').innerHTML = discountPrice40;
+    console.log('Prezzo scontato: ', discountPrice40);
     document.getElementById('baseprice2').innerHTML = ('al posto di € ' + tripPrice);
 }
