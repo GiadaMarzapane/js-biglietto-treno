0a65d624b6f2feec31167a5fa11c52ffe0738c08
GiadaMarzapane <giada.ortesta@gmail.com>
2022-12-19 16:30:49 +0100

    Fix e fine esercizio base

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const kmChoice = prompt ('Quanti kilometri devi percorrere?');
+const kmChoice = prompt('Quanti kilometri devi percorrere?');
 console.log('I kilometri da percorrere sono: ', kmChoice);
 console.log(typeof 'I kilometri da percorrere sono: ', kmChoice);
 
@@ -8,6 +8,9 @@ console.log(typeof "L'utente ha: ", myAge);
 
 let tripPrice = kmChoice * 0.21;
 tripPrice = tripPrice.toFixed(2);
+
+console.log('Il prezzo del biglietto sarà €', tripPrice);
+console.log(typeof 'Il prezzo base del biglietto sarà €', tripPrice);
 
 let discountPrice20 = (tripPrice - (tripPrice * 0.20));
 discountPrice20 = discountPrice20.toFixed(2);
@@ -19,13 +22,15 @@ console.log('Il prezzo del biglietto sarà €', tripPrice);
 console.log(typeof 'Il prezzo base del biglietto sarà €', tripPrice);
 
 if (myAge < 18) {
-    document.getElementById('minorenne').innerHTML= discountPrice20;
+    document.getElementById('minorenne').innerHTML = discountPrice20;
     console.log('Prezzo scontato: ', discountPrice20);
-    document.getElementById('baseprice').innerHTML = ('al posto di € ' + tripPrice);
+    document.getElementById('baseprice1').innerHTML = ('al posto di € ' + tripPrice);
 }
-
-if (myAge >= 65) {
+else if (myAge >= 65) {
     document.getElementById('over65').innerHTML = discountPrice40;
     console.log('Prezzo scontato: ', discountPrice40);
     document.getElementById('baseprice2').innerHTML = ('al posto di € ' + tripPrice);
+}
+else {
+    document.getElementById('baseprice').innerHTML = tripPrice;
 }
