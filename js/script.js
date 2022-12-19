fa9fd117242f7d9e0a786497db55cfdd33692a23
GiadaMarzapane <giada.ortesta@gmail.com>
2022-12-19 15:39:02 +0100

    Fine esercizio

--- js/script.js
+++ js/script.js
@@ -4,18 +4,22 @@ console.log(typeof 'I kilometri da percorrere sono: ', kmChoice);
 
 const myAge = prompt('Quanti anni hai?');
 console.log("L'utente ha: ", myAge);
-console.log(typeof "L'utente ha: ", myAge);
+console.log(typeof "L'utente ha: ", myAge, 'anni');
 
-const tripPrice = kmChoice * 0.21;
+let tripPrice = kmChoice * 0.21;
+tripPrice = tripPrice.toFixed(2);
+
 console.log('Il prezzo del biglietto sarà €', tripPrice);
-console.log(typeof 'Il prezzo del biglietto sarà €', tripPrice);
+console.log(typeof 'Il prezzo base del biglietto sarà €', tripPrice);
 
 if (myAge < '18') {
-    document.getElementById('maggiorenne').innerHTML= (tripPrice - (tripPrice * 0.20));
+    document.getElementById('minorenne').innerHTML= (tripPrice - (tripPrice * 0.20));
     console.log(tripPrice - (tripPrice * 0.20));
+    document.getElementById('baseprice').innerHTML = ('al posto di € ' + tripPrice);
 }
 
 if (myAge >= '65') {
     document.getElementById('over65').innerHTML = (tripPrice - (tripPrice * 0.40));
     console.log(tripPrice - (tripPrice * 0.40));
+    document.getElementById('baseprice2').innerHTML = ('al posto di € ' + tripPrice);
 }
