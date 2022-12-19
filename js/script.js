346dc5b83b3ea79b61caf65f3f7c24c6e96b7467
GiadaMarzapane <giada.ortesta@gmail.com>
2022-12-19 15:07:27 +0100

    Dichiarate prime due const

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const kmChoice = prompt ('Quanti kilometri devi percorrere?');
+console.log('I kilometri da percorrere sono: ', kmChoice);
+
+const myAge = prompt('Quanti anni hai?');
+console.log("L'utente ha: ", myAge);
